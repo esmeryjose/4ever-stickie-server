0293b280078ba9a0cf3aca164a66c9de1a991e9d
esmeryjose <esmerycornielle@gmail.com>
2017-08-04 19:33:25 -0400

    added seed file as well as note model association

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+notes1 = ["this is a note", "this is another note", "i want to fly", "we code everyday", "we are not broke anymore"]
+notes2 = ["this is a short note", "this is another short note", "i want to swim in the ocean", "i wish i could code everyday", "we used to be very broke, now we are not"]
+boards = ["random","boats"]
+
+boards.each_with_index do |board,i|
+  newBoard = Board.create(title: board)
+  if i == 0
+    notes1.each do |note|
+      newBoard.notes << Note.create(content: note)
+    end
+  else
+    notes2.each do |note|
+      newBoard.notes << Note.create(content: note)
+    end
+  end
+end
