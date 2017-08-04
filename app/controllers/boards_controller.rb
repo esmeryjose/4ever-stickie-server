9e328a20028d1d6e2bbf02448d85df5276fab4e5
esmeryjose <esmerycornielle@gmail.com>
2017-08-04 17:54:38 -0400

    boards controller with basic actions completed

---
+++ app/controllers/boards_controller.rb
@@ -0,0 +1,32 @@
+class BoardsController < ApplicationController
+  before_action :set_board, only:[:show,:update]
+
+  def index
+    boards = Board.all
+    render json: boards
+  end
+
+  def create
+    board = Board.create(board_params)
+    render json: board
+  end
+
+  def show
+    render json: board
+  end
+
+  def update
+    board = Board.update(board_params)
+    render json: board
+  end
+
+  private
+
+  def board_params
+    params.require(:board).permit(:title, :user_id)
+  end
+
+  def set_board
+    board = Board.find_by(id: params[:board][:id])
+  end
+end
